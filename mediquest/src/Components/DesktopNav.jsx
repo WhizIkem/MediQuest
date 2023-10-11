358b9a17d7a6418719654c3ed2422adb0ff8a137
whizikem <wisdomikem@gmail.com>
2023-10-11 22:32:06 +0100

    Header components created

---
+++ mediquest/src/Components/DesktopNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const DesktopNav = () => {
+  return (
+    <nav className="desktop-nav">
+      <ul>
+        <li>Home</li>
+        <li>Symptom Checker</li>
+        <li>About Us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default DesktopNav;
