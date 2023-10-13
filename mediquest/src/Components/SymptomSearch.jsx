bb1a33a676b477aff9ad96dfaae58ac4c5ea3dad
whizikem <wisdomikem@gmail.com>
2023-10-13 17:23:44 +0100

    added components for the about us, diseases, and symptoms

--- mediquest/src/Components/SymptomSearch.jsx
+++ mediquest/src/Components/SymptomSearch.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+// SymptomSearch.jsx
+
+import React, { useState, useEffect } from "react";
 import PlusIcon from "../Assets/plusIcon.svg";
 
 const SymptomSearch = ({ setSelectedSymptoms }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  const allSymptoms = ["Fever", "Cough", "Headache", "Sore throat", "Runny nose", "Fatigue", "Muscle pain", "Diarrhea", "Loss of taste or smell"]; //fetch from API
+  const [allSymptoms, setAllSymptoms] = useState([]);
+
+  useEffect(() => {
+    const fetchSymptoms = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/all_symptoms');
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        setAllSymptoms(data.all_symptoms);
+      } catch (error) {
+        console.error('There was a problem with the fetch operation:', error.message);
+      }
+    };
+
+    fetchSymptoms();
+  }, []);
 
   const suggestions = allSymptoms.filter(symptom => symptom.toLowerCase().includes(searchTerm.toLowerCase()));
 
@@ -21,13 +42,13 @@ const SymptomSearch = ({ setSelectedSymptoms }) => {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       {searchTerm &&
-      <div className="suggestions">
-        {suggestions.map((symptom, index) => (
-          <div key={index} onClick={() => handleSelectedSymptoms(symptom)}>
-            {symptom} <img src={PlusIcon} alt="plus icon" />
-          </div>
-        ))}
-      </div>
+        <div className="suggestions">
+          {suggestions.map((symptom, index) => (
+            <div key={index} onClick={() => handleSelectedSymptoms(symptom)}>
+              {symptom} <img src={PlusIcon} alt="plus icon" />
+            </div>
+          ))}
+        </div>
       }
     </div>
   );
---
+++ mediquest/src/Components/SymptomsChecker.jsx
@@ -0,0 +1,43 @@
+// SymptomChecker.jsx
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const SymptomChecker = ({ selectedSymptoms = [] }) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/suggest_diseases', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ selected_symptoms: selectedSymptoms }),
+      });
+  
+      console.log("Response status:", response.status);
+  
+      const data = await response.json();
+      console.log("Data received:", data);
+  
+      navigate("/diseases", { state: { diseases: data } });
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error.message);
+    }
+  }
+  
+
+  return (
+    <div>
+      <div>
+        {selectedSymptoms && selectedSymptoms.map((symptom, index) => (
+          <span key={index}>{symptom}</span>
+        ))}
+      </div>
+      <button onClick={handleSubmit}>Check Symptoms</button>
+    </div>
+  );
+}
+
+export default SymptomChecker;
