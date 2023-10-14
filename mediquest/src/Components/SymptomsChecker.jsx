b5471a3e5458048120d70beaa51c6991a0a72d75
Festus Omaku <fomaku3@gmail.com>
2023-10-14 21:58:53 +0100

    still fixing the diseases page

--- mediquest/src/Components/SymptomsChecker.jsx
+++ mediquest/src/Components/SymptomsChecker.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+// ... (other imports)
+
 const SymptomChecker = ({ selectedSymptoms = [] }) => {
   const navigate = useNavigate();
 
@@ -16,15 +18,23 @@ const SymptomChecker = ({ selectedSymptoms = [] }) => {
 
       console.log("Response status:", response.status);
 
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
       const data = await response.json();
       console.log("Data received:", data);
 
-      navigate("/diseases", { state: { diseases: data } });
+      // Check if 'suggested_diseases' is available in the response
+      if (data && data.suggested_diseases) {
+        navigate("/diseases", { state: { diseases: data.suggested_diseases } });
+      } else {
+        console.error('No suggested diseases found in the response');
+      }
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error.message);
     }
   }
-
 
   return (
     <div>
@@ -32,10 +42,11 @@ const SymptomChecker = ({ selectedSymptoms = [] }) => {
         {selectedSymptoms && selectedSymptoms.map((symptom, index) => (
           <span key={index}>{symptom}</span>
         ))}
-           </div>
+      </div>
       <button onClick={handleSubmit}>Check Symptoms</button>
     </div>
   );
 }
 
 export default SymptomChecker;
+
