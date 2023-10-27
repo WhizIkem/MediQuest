58a26f4586d88050dd7fcd564608008e0c16c089
Festus Omaku <fomaku3@gmail.com>
2023-10-27 09:40:15 +0100

    clean up

--- mediquest_backend/app/location.py
+++ mediquest_backend/app/location.py
@@ -17,7 +17,7 @@ def calculate_distance(coord1, coord2):
     a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
     c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-    # Calculate the distance
+    # Calculating the distance
     distance = earth_radius * c
     return distance
 
@@ -36,17 +36,15 @@ def get_user_location():
 
 # Function to find the nearest health facilities using the HERE Location Services API
 def find_nearest_health_facilities(latitude, longitude):
-    # Define the endpoint URL for the HERE Location Services API
+    #URL for the Location Services API
     search_url = 'https://discover.search.hereapi.com/v1/discover'
-
-    # Replace 'YOUR_HERE_API_KEY' with your actual HERE Location Services API key.
     api_key = 'AoN49OcFKSSbwmOrCVau3IIQiWHqc0qVQ0eAEmqGTjY'
 
-    # Define the facility types to search for
+    #facility types to search for
     facility_types = ["hospital", "clinic"]
 
     for facility_type in facility_types:
-        # Set the parameters for the API request to search for health facilities
+        # parameters for the API request to search for health facilities
         facility_params = {
             'apiKey': api_key,
             'at': f"{latitude},{longitude}",
@@ -54,19 +52,19 @@ def find_nearest_health_facilities(latitude, longitude):
             'limit': 5,
         }
 
-        # Make the API request to search for nearby health facilities
+        # API request to search for nearby health facilities
         facility_response = requests.get(search_url, params=facility_params)
 
         if facility_response.status_code == 200:
             facility_data = facility_response.json()
-            # Process the data to find and handle the nearest health facilities of the specified type
+            
             items = facility_data.get('items', [])
             if items:
                 print(f"Nearby {facility_type}s:")
                 for item in items:
                     title = item.get('title', 'Facility name not found')
                     address = item.get('address', 'Address not found')
-                    position = item.get('position', {'lat': 0, 'lng': 0})  # Get the position
+                    position = item.get('position', {'lat': 0, 'lng': 0}
                     distance = calculate_distance((latitude, longitude), (position['lat'], position['lng']))
                     print(f"Name: {title}, Distance: {int(distance)} meters")
                     print(f"Address: {address}")
@@ -76,7 +74,7 @@ def find_nearest_health_facilities(latitude, longitude):
         else:
             print(f"Request to HERE Location Services for {facility_type}s failed.")
 
-# Call the function to get the user's location
+# function to get the user's location
 user_location = get_user_location()
 
 if user_location:
@@ -84,7 +82,7 @@ if user_location:
     print(f"Latitude: {latitude}")
     print(f"Longitude: {longitude}")
 
-    # Call the function to find the nearest health facilities
+    # function to find the nearest health facilities
     find_nearest_health_facilities(latitude, longitude)
 else:
     print("Location not available.")
